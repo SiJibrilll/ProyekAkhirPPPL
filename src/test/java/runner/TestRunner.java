197f728cd75ef10af7686960e6691150145af526
SiJibrilll <jibrilianwa@gmail.com>
2026-05-28 14:58:02 +0700

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package runner;
+
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "@positive"
+)
+public class TestRunner {
+}
